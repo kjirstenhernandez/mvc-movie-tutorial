5550226b0a133332490e7c59bcf3be2e1d440dc0
Kjirsten Hernandez <sandlot7665@gmail.com>
2024-11-15 15:28:31 -0700

    first commit

---
+++ Models/SeedData.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            };
+            context.SaveChanges();
+        }
+    }
+}
